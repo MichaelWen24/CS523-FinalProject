5d4f0976888fcbfcd3198c798d89efa11a736b57
MicahelWen318 <wen820061663@gmail.com>
2019-04-08 22:33:45 -0400

    Initial Commit

---
+++ iOSPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSPlayer
+//
+//  Created by Michael Wen on 4/8/19.
+//  Copyright © 2019 Yining Wen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
